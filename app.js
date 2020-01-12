9de19707db671ecbfb83ba7d5dbb8b2e060249e4
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-12 00:21:16 -0500

    Template literals for engineer role

--- app.js
+++ app.js
@@ -101,3 +101,34 @@ function promptUser() {
         console.error("Unable to write to engineer file.");
       };
     }
+
+    else if (response.role === 'Engineer') {
+        const github = roleQ.github;
+        //const engineer = new Engineer(response.name, response.id, response.email, github);
+        //console.log(engineer);
+        const engineerCard =
+          `<div class="col-4">
+                  <div class="card bg-primary mb-3" style="max-width: 18rem;">
+                      <div class="card-header text-white">
+                          <h5 class="card-title">${response.name}</h5>
+                          <h5 class="card-subtitle">Engineer</h5>
+                      </div>
+                      <div class="card-body bg-light">
+                          <div class="bg-white m-3">
+                              <ul class="list-group">
+                                  <li class="list-group-item">ID: ${response.id}</li>
+                                  <li class="list-group-item">Email: ${response.email}</span></li>
+                                  <li class="list-group-item">Github: ${github}</li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>`;
+    
+        try {
+          fs.appendFileSync("./output/main.html", `${engineerCard}`);
+        }
+        catch {
+          console.error("Unable to write to engineer file.");
+        }
+      }
