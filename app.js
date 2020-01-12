a995af9d2bd2628c97bcbb96e5322acdf2ec2675
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-12 00:22:06 -0500

    Template literals for intern role

--- app.js
+++ app.js
@@ -132,3 +132,35 @@ function promptUser() {
           console.error("Unable to write to engineer file.");
         }
       }
+
+      else if (response.role === 'Intern') {
+        const school = roleQ.school;
+        //const intern = new Intern(response.name, response.id, response.email, school);
+        //console.log(intern);
+        const engineerCard =
+          `<div class="col-4">
+                  <div class="card bg-primary mb-3" style="max-width: 18rem;">
+                      <div class="card-header text-white">
+                          <h5 class="card-title">${response.name}</h5>
+                          <h5 class="card-subtitle">Intern</h5>
+                      </div>
+                      <div class="card-body bg-light">
+                          <div class="bg-white m-3">
+                              <ul class="list-group">
+                                  <li class="list-group-item">ID: ${response.id}</li>
+                                  <li class="list-group-item">Email: ${response.email}</span></li>
+                                  <li class="list-group-item">School: ${school}</li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>`;
+    
+        try {
+          fs.appendFileSync("./output/main.html", `${engineerCard}`);
+        }
+        catch {
+          console.error("Unable to write to engineer file.");
+        }
+      }
+    }
