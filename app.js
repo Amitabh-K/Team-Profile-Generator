6e44598e302411451aa4319c772dc76964534332
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-12 00:13:35 -0500

    adding user input  function with inquirer

--- app.js
+++ app.js
@@ -4,3 +4,35 @@ const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
 const inquirer = require("inquirer");
 const fs = require("fs");
+
+
+function promptUser() {
+    return inquirer
+      .prompt([
+        {
+          message: "Please enter your name",
+          name: "name",
+          type: "input"
+        },
+        {
+          message: "Please enter your id",
+          name: "id",
+          type: "number"
+        },
+        {
+          message: "Please enter your email",
+          name: "email",
+          type: "input"
+        },
+        {
+          message: "Please enter your Role",
+          name: "role",
+          type: "list",
+          choices: [
+            'Manager',
+            'Engineer',
+            'Intern',
+          ]
+        }
+      ]);
+  }
