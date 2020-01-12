4d4ec227efa87f9ec1864c475a7a9342616be019
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-12 00:19:06 -0500

    Template literals for manager role

--- app.js
+++ app.js
@@ -68,3 +68,36 @@ function promptUser() {
       ])
     }
   }
+
+
+  function addCards(response, roleQ) {
+    if (response.role === 'Manager') {
+      const officenumber = roleQ.officenumber;
+      //const manager = new Manager(response.name, response.id, response.email, officenumber);
+      //console.log(manager);
+      const managerCard =
+        `<div class="col-4">
+                <div class="card bg-primary mb-3" style="max-width: 18rem;">
+                    <div class="card-header text-white">
+                        <h5 class="card-title">${response.name}</h5>
+                        <h5 class="card-subtitle">Manager</h5>
+                    </div>
+                    <div class="card-body bg-light">
+                        <div class="bg-white m-3">
+                            <ul class="list-group">
+                                <li class="list-group-item">ID: ${response.id}</li>
+                                <li class="list-group-item">Email: ${response.email}</span></li>
+                                <li class="list-group-item">Office Number: ${officenumber}</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>`;
+  
+      try {
+        fs.appendFileSync("./output/main.html", `${managerCard}`);
+      }
+      catch {
+        console.error("Unable to write to engineer file.");
+      };
+    }
