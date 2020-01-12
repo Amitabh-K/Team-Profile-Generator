44d6458a99ec8f9054ee29ac039ccc10a5b51154
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-12 00:11:34 -0500

    adding npm require

---
+++ app.js
@@ -0,0 +1,6 @@
+const Employee = require("./lib/employee");
+const Engineer = require("./lib/engineer");
+const Manager = require("./lib/manager");
+const Intern = require("./lib/intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
