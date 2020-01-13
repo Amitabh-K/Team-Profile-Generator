ef5e482f3520b67c3c994bab48688bae63778a22
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-01-12 20:55:43 -0500

    merged emp cards to app file

---
+++ Assets/src/app.js
@@ -0,0 +1,85 @@
+$('.port-item').click(function () {
+    $('.collapse').collapse('hide');
+  });
+
+  $(document).on('click', '[data-toggle="lightbox"]', function (e) {
+    e.preventDefault();
+    $(this).ekkoLightbox();
+  });
+
+  
+
+// Script for typewriter animation
+// List of sentences
+var _CONTENT = [
+	"Amitabh Kumar",
+	"🚂JavaScrip, 🚃Node.js",
+	"🚃React.js, 🚃AngularJS",  
+	"🚉MongoDB, 🚉MySQL",
+  	"💽Github"
+];
+
+// Current sentence being processed
+var _PART = 0;
+
+// Character number of the current sentence being processed
+var _PART_INDEX = 0;
+
+// Holds the handle returned from setInterval
+var _INTERVAL_VAL;
+
+// Element that holds the text
+var _ELEMENT = document.querySelector("#text");
+
+// Cursor element
+var _CURSOR = document.querySelector("#cursor");
+
+// Implements typing effect
+function Type() {
+	// Get substring with 1 characater added
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX + 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX++;
+
+	// If full sentence has been displayed then start to delete the sentence after some time
+	if(text === _CONTENT[_PART]) {
+		// Hide the cursor
+		_CURSOR.style.display = 'none';
+
+		clearInterval(_INTERVAL_VAL);
+		setTimeout(function() {
+			_INTERVAL_VAL = setInterval(Delete, 50);
+		}, 1000);
+	}
+}
+
+// Implements deleting effect
+function Delete() {
+	// Get substring with 1 characater deleted
+	var text =  _CONTENT[_PART].substring(0, _PART_INDEX - 1);
+	_ELEMENT.innerHTML = text;
+	_PART_INDEX--;
+
+	// If sentence has been deleted then start to display the next sentence
+	if(text === '') {
+		clearInterval(_INTERVAL_VAL);
+
+		// If current sentence was last then display the first one, else move to the next
+		if(_PART == (_CONTENT.length - 1))
+			_PART = 0;
+		else
+			_PART++;
+
+		_PART_INDEX = 0;
+
+		// Start to display the next sentence after some time
+		setTimeout(function() {
+			_CURSOR.style.display = 'inline-block';
+			_INTERVAL_VAL = setInterval(Type, 100);
+		}, 200);
+	}
+}
+
+// Start the typing effect on load
+_INTERVAL_VAL = setInterval(Type, 100);
+
--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
 const Manager = require("./lib/manager");
 const Intern = require("./lib/intern");
+const addCards = require("./lib/addEmployee");
 const inquirer = require("inquirer");
 const fs = require("fs");
 const open = require ("open")
@@ -94,102 +95,101 @@ function promptUser() {
     }
   }
 
-
   function addCards(response, roleQ) {
-    if (response.role === 'Manager') {
-      const officenumber = roleQ.officenumber;
-      //const manager = new Manager(response.name, response.id, response.email, officenumber);
-      //console.log(manager);
-      const managerCard =
-        `<div class="col-4">
-                <div class="card bg-primary mb-3" style="max-width: 18rem;">
-                    <div class="card-header text-white">
-                        <h5 class="card-title">${response.name}</h5>
-                        <h5 class="card-subtitle">Manager</h5>
-                    </div>
-                    <div class="card-body bg-light">
-                        <div class="bg-white m-3">
-                            <ul class="list-group">
-                                <li class="list-group-item">ID: ${response.id}</li>
-                                <li class="list-group-item">Email: ${response.email}</span></li>
-                                <li class="list-group-item">Office Number: ${officenumber}</li>
-                            </ul>
+      if (response.role === 'Manager') {
+        const officenumber = roleQ.officenumber;
+        //const manager = new Manager(response.name, response.id, response.email, officenumber);
+        //console.log(manager);
+        const managerCard =
+          `<div class="col-4">
+                  <div class="card bg-primary mb-3" style="max-width: 18rem;">
+                      <div class="card-header text-white">
+                          <h5 class="card-title">${response.name}</h5>
+                          <h5 class="card-subtitle">Manager</h5>
+                      </div>
+                      <div class="card-body bg-light">
+                          <div class="bg-white m-3">
+                              <ul class="list-group">
+                                  <li class="list-group-item">ID: ${response.id}</li>
+                                  <li class="list-group-item">Email: ${response.email}</span></li>
+                                  <li class="list-group-item">Office Number: ${officenumber}</li>
+                              </ul>
+                          </div>
+                      </div>
+                  </div>
+              </div>`;
+    
+        try {
+          fs.appendFileSync("./output/main.html", `${managerCard}`);
+        }
+        catch {
+          console.error("Unable to write to engineer file.");
+        };
+      }
+  
+      else if (response.role === 'Engineer') {
+          const github = roleQ.github;
+          //const engineer = new Engineer(response.name, response.id, response.email, github);
+          //console.log(engineer);
+          const engineerCard =
+            `<div class="col-4">
+                    <div class="card bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header text-white">
+                            <h5 class="card-title">${response.name}</h5>
+                            <h5 class="card-subtitle">Engineer</h5>
+                        </div>
+                        <div class="card-body bg-light">
+                            <div class="bg-white m-3">
+                                <ul class="list-group">
+                                    <li class="list-group-item">ID: ${response.id}</li>
+                                    <li class="list-group-item">Email: ${response.email}</span></li>
+                                    <li class="list-group-item">Github: ${github}</li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-            </div>`;
+                </div>`;
+      
+          try {
+            fs.appendFileSync("./output/main.html", `${engineerCard}`);
+          }
+          catch {
+            console.error("Unable to write to engineer file.");
+          }
+        }
   
-      try {
-        fs.appendFileSync("./output/main.html", `${managerCard}`);
-      }
-      catch {
-        console.error("Unable to write to engineer file.");
-      };
-    }
-
-    else if (response.role === 'Engineer') {
-        const github = roleQ.github;
-        //const engineer = new Engineer(response.name, response.id, response.email, github);
-        //console.log(engineer);
-        const engineerCard =
-          `<div class="col-4">
-                  <div class="card bg-primary mb-3" style="max-width: 18rem;">
-                      <div class="card-header text-white">
-                          <h5 class="card-title">${response.name}</h5>
-                          <h5 class="card-subtitle">Engineer</h5>
-                      </div>
-                      <div class="card-body bg-light">
-                          <div class="bg-white m-3">
-                              <ul class="list-group">
-                                  <li class="list-group-item">ID: ${response.id}</li>
-                                  <li class="list-group-item">Email: ${response.email}</span></li>
-                                  <li class="list-group-item">Github: ${github}</li>
-                              </ul>
-                          </div>
-                      </div>
-                  </div>
-              </div>`;
-    
-        try {
-          fs.appendFileSync("./output/main.html", `${engineerCard}`);
-        }
-        catch {
-          console.error("Unable to write to engineer file.");
+        else if (response.role === 'Intern') {
+          const school = roleQ.school;
+          //const intern = new Intern(response.name, response.id, response.email, school);
+          //console.log(intern);
+          const engineerCard =
+            `<div class="col-4">
+                    <div class="card bg-primary mb-3" style="max-width: 18rem;">
+                        <div class="card-header text-white">
+                            <h5 class="card-title">${response.name}</h5>
+                            <h5 class="card-subtitle">Intern</h5>
+                        </div>
+                        <div class="card-body bg-light">
+                            <div class="bg-white m-3">
+                                <ul class="list-group">
+                                    <li class="list-group-item">ID: ${response.id}</li>
+                                    <li class="list-group-item">Email: ${response.email}</span></li>
+                                    <li class="list-group-item">School: ${school}</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>`;
+      
+          try {
+            fs.appendFileSync("../output/main.html", `${engineerCard}`);
+          }
+          catch {
+            console.error("Unable to write to engineer file.");
+          }
         }
       }
-
-      else if (response.role === 'Intern') {
-        const school = roleQ.school;
-        //const intern = new Intern(response.name, response.id, response.email, school);
-        //console.log(intern);
-        const engineerCard =
-          `<div class="col-4">
-                  <div class="card bg-primary mb-3" style="max-width: 18rem;">
-                      <div class="card-header text-white">
-                          <h5 class="card-title">${response.name}</h5>
-                          <h5 class="card-subtitle">Intern</h5>
-                      </div>
-                      <div class="card-body bg-light">
-                          <div class="bg-white m-3">
-                              <ul class="list-group">
-                                  <li class="list-group-item">ID: ${response.id}</li>
-                                  <li class="list-group-item">Email: ${response.email}</span></li>
-                                  <li class="list-group-item">School: ${school}</li>
-                              </ul>
-                          </div>
-                      </div>
-                  </div>
-              </div>`;
-    
-        try {
-          fs.appendFileSync("./output/main.html", `${engineerCard}`);
-        }
-        catch {
-          console.error("Unable to write to engineer file.");
-        }
-      }
-    }
-
+      
 // 🔁Decision exit or remain in the loop 
     function addEmp() {
         return inquirer.prompt([
